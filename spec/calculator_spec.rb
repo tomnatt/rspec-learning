8add17504c9f242a94d964135ae5bf72f5b3a799
tomnatt <github@tomnatt.com>
2020-09-14 17:28:15 +0100

    Fixed some rubocop violations

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,10 +1,14 @@
 require './calculator'
 
 describe 'calculator' do
-  it 'adds numbers' do
+  it 'adds two numbers' do
     calc = Calculator.new
-    expect(calc.add(5, 3)).to eql(8)
-    expect(calc.add(2, 3)).to eql(5)
+    expect(calc.add(5, 3)).to be(8)
+  end
+
+  it 'adds two other numbers' do
+    calc = Calculator.new
+    expect(calc.add(2, 3)).to be(5)
   end
 
   it 'returns an integer' do
@@ -12,10 +16,18 @@ describe 'calculator' do
     expect(calc.add(2, 3)).to be_instance_of(Integer)
   end
 
-  it 'raises an error if given incorrect type' do
+  it 'raises an error if given two incorrect types' do
     calc = Calculator.new
     expect { calc.add('foo', 'bar') }.to raise_error(TypeError)
+  end
+
+  it 'raises an error if first argument is incorrect type' do
+    calc = Calculator.new
     expect { calc.add('foo', 2) }.to raise_error(TypeError)
+  end
+
+  it 'raises an error if second argument is incorrect type' do
+    calc = Calculator.new
     expect { calc.add(4, 'bar') }.to raise_error(TypeError)
   end
 end
